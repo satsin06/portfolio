4574e4385935acb59cd99de29b0d28c331631f4a
Satyam Sinha <satyamsinha9404@gmail.com>
2021-11-18 00:27:02 +0530

    completed appbar

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -11,100 +11,116 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
+    var screenSize = MediaQuery.of(context).size;
     return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size(screenSize.width, 1000),
+        child: appBar(),
+      ),
       body: SingleChildScrollView(
         child: Column(
-          children: [
-            topBar(),
+          children: const [
+            //topBar(),
           ],
         ),
       ),
     );
   }
-  Widget topBar() {
+
+  Widget appBar() {
+    var screenSize = MediaQuery.of(context).size;
     const speed = Duration(milliseconds: 200);
-    return SizedBox(
-      height: MediaQuery.of(context).size.height * 0.52,
-      width: MediaQuery.of(context).size.width,
-      child: Stack(
-        children: [
-          Image.asset(
+    return Container(
+        height: screenSize.height / 1.5,
+        decoration: const BoxDecoration(
+            image: DecorationImage(
+          image: AssetImage(
             'assets/background.jpg',
-            fit: BoxFit.cover,
-            width: double.infinity,
           ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              AnimatedTextKit(
-                totalRepeatCount: 1,
-                animatedTexts: [
-                  TypewriterAnimatedText(
-                    'SATYAM SINHA',
-                    textStyle: const TextStyle(
-                      color: Colors.redAccent,
-                      fontSize: 32.0,
-                      fontWeight: FontWeight.bold,
-                    ),
-                    speed: const Duration(milliseconds: 500),
+          fit: BoxFit.cover,
+        )),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            SizedBox(
+              height: screenSize.width / 40,
+            ),
+            AnimatedTextKit(
+              totalRepeatCount: 2,
+              animatedTexts: [
+                TypewriterAnimatedText(
+                  'SATYAM SINHA',
+                  textStyle: const TextStyle(
+                    color: Colors.redAccent,
+                    fontSize: 32.0,
+                    fontWeight: FontWeight.bold,
                   ),
-                ],
-              ),
-              const Align(
-                alignment: Alignment.topCenter,
-                child: CircleAvatar(
-                  radius: 60.0,
-                  backgroundImage: AssetImage('assets/avatar.jpg'),
+                  speed: const Duration(milliseconds: 500),
                 ),
+              ],
+            ),
+            const Align(
+              alignment: Alignment.topCenter,
+              child: CircleAvatar(
+                radius: 60.0,
+                backgroundImage: AssetImage('assets/avatar.jpg'),
               ),
-              Column(
-                children: [
-                  Center(
-                    child: SizedBox(
-                      width: 500,
-                      height: 100,
-                      child: DefaultTextStyle(
-                        style: const TextStyle(
-                          fontSize: 40,
-                          color: Colors.red,
+            ),
+            // SizedBox(
+            //   height: screenSize.width / 16,
+            // ),
+            SizedBox(
+              width: screenSize.width / 3,
+              //height: 100,
+              child: DefaultTextStyle(
+                style: const TextStyle(
+                  //fontSize: 32,
+                  color: Colors.red,
+                ),
+                child: SizedBox(
+                  height: screenSize.height / 6,
+                  child: FittedBox(
+                    fit: BoxFit.fitWidth,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Text(
+                          'I am ',
+                          maxLines: 1,
+                          style: TextStyle(color: Colors.white),
                         ),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            const Text('I am ', style: TextStyle(color: Colors.white),),
-                            AnimatedTextKit(
-                              repeatForever: true,
-                              animatedTexts: [
-                                TypewriterAnimatedText('SATYAM', speed: speed),
-                                TypewriterAnimatedText('PROGRAMMER', speed: speed),
-                                TypewriterAnimatedText('DEVELOPER', speed: speed),
-                                TypewriterAnimatedText("SATSIN06", speed: speed),
-                              ],
-                            ),
+                        AnimatedTextKit(
+                          repeatForever: true,
+                          animatedTexts: [
+                            TypewriterAnimatedText('SATYAM', speed: speed),
+                            TypewriterAnimatedText('PROGRAMMER', speed: speed),
+                            TypewriterAnimatedText('DEVELOPER', speed: speed),
+                            TypewriterAnimatedText("SATSIN06", speed: speed),
                           ],
                         ),
-                      ),
+                      ],
                     ),
                   ),
-                  const SizedBox(
-                    height: 40,
-                  ),
-                  const SizedBox(
-                    width: 500.0,
-                    child: Center(
-                        child: Text('Flutter Developer', style: TextStyle(
-                          color: Colors.white54,
-                          fontSize: 28.0,
-                          fontFamily: 'Horizon',
-                        ),)
-                    ),
-                  ),
-                ],
-              )
-            ],
-          )
-        ],
-      ),
-    );
+                ),
+              ),
+            ),
+            const SizedBox(
+              width: 500.0,
+              child: Center(
+                  child: FittedBox(
+                    fit: BoxFit.fitWidth,
+                    child: Text(
+                'Flutter Developer',
+                maxLines: 1,
+                style: TextStyle(
+                    color: Colors.white54,
+                    fontSize: 28.0,
+                    fontFamily: 'Horizon',
+                ),
+              ),
+                  )),
+            )
+          ],
+        ));
   }
 }
